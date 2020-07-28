e8b8e6429c5f859b66d6cccf4d25c92849980ef7
luo-h-x <1877430627@qq.com>
2020-07-28 21:31:06 +0800

    element

---
+++ src/plugins/elements.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import {
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Progress,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Carousel,
+  CarouselItem
+} from 'element-ui'
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Progress)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
