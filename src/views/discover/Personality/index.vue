1c25be859a9d41fbd40d1646b57cef62fbcc5b7f
luo-h-x <1877430627@qq.com>
2020-08-08 22:23:54 +0800

    resonality

---
+++ src/views/discover/Personality/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="personality-page">
+    <Banner/>
+    <recommended-song/>
+    <sole/>
+    <latest-music/>
+    <music-video/>
+    <radio-station/>
+  </div>
+</template>
+
+<script>
+import Banner from './Banner'
+import RecommendedSong from './RecommendedSong'
+import Sole from './Sole'
+import LatestMusic from './LatestMusic'
+import MusicVideo from './MusicVideo'
+import RadioStation from './RadioStation'
+export default {
+  components: {
+    Banner,
+    RecommendedSong,
+    Sole,
+    LatestMusic,
+    MusicVideo,
+    RadioStation
+  }
+}
+</script>
+
+<style lang="scss" >
+.s-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px #e6e6e6 solid;
+  margin: 20px 0;
+  > h3 {
+    line-height: 40px;
+    font-weight: 400;
+  }
+  > span {
+    font-size: 12px;
+    cursor: pointer;
+  }
+}
+</style>
