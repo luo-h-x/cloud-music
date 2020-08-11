c837c47dc6a5f5189bfcff6e42e61632322e1dff
luo-h-x <1877430627@qq.com>
2020-08-11 21:45:41 +0800

    al-deatils

---
+++ src/views/Album/Details.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="deatils">
+    <h3 class="ss-tit">专辑介绍</h3>
+    <p class="si-content" v-for="(val, index) in description" :key="index">
+      {{ val }}
+    </p>
+  </div>
+</template>
+
+<script>
+import api from '../../api/discover'
+export default {
+  data() {
+    return {
+      description: ''
+    }
+  },
+  mounted() {
+    // 获取歌单列表
+    api.getAlbum(this.$route.query.id).then(val => {
+      this.description = val.data.album.description.split('\n')
+    })
+  }
+}
+</script>
+
+<style>
+.deatils {
+  margin: 10px 0;
+}
+.ss-tit {
+  font-size: 14px;
+}
+.si-content {
+  line-height: 28px;
+  text-indent: 28px;
+}
+</style>
