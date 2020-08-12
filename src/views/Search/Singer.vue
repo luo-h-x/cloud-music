db2262f059afeca6ce3e650ff342638045e317f2
luo-h-x <1877430627@qq.com>
2020-08-12 21:41:25 +0800

    s-singer

---
+++ src/views/Search/Singer.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul class="singer-album">
+    <li
+      class="singer-album-item"
+      v-for="(item, index) in result.artists"
+      :key="index"
+    >
+      <img :src="item.img1v1Url" class="sa-pic" />
+      <router-link
+        :to="{ path: '/Singer', query: { id: item.id } }"
+        class="sa-title"
+        >{{ item.name }}</router-link
+      >
+      <!-- <a href="javascript:;" class="sa-title">{{ item.name }}</a> -->
+    </li>
+  </ul>
+</template>
+
+<script>
+import api from '../../api/discover'
+export default {
+  data() {
+    return {
+      result: []
+    }
+  },
+  mounted() {
+    api.getSearch(this.$route.query.keyword, 100).then(val => {
+      this.result = val.data.result
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.singer-album {
+  .singer-album-item {
+    display: grid;
+    grid-template-columns: 0.5fr 3fr 3fr 3fr;
+    grid-column-gap: 10px;
+    align-items: center;
+    margin: 10px 0;
+    font-size: 14px;
+    color: #999;
+    height: 50px;
+    &:nth-child(even) {
+      background: #fafafa;
+    }
+    &:hover {
+      background: rgba(0, 0, 0, 0.1) !important;
+    }
+    .sa-pic {
+      width: 50px;
+    }
+  }
+}
+</style>
