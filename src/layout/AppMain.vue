d03ca23a4d2b93f25bccd9de2e79990241f4d125
luo-h-x <1877430627@qq.com>
2020-08-25 19:25:02 +0800

    添加回到顶部

--- src/layout/AppMain.vue
+++ src/layout/AppMain.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="appMain">
+    <el-backtop
+      target=".main-scrollbar .el-scrollbar__wrap"
+      :bottom="60"
+    >
+      <img src="../assets/backtop.svg" >
+    </el-backtop>
     <keep-alive>
-      <router-view v-if="this.$route.meta.keepAlive" :key="$route.fullPath" ></router-view>
+      <router-view
+        v-if="this.$route.meta.keepAlive"
+        :key="$route.fullPath"
+      ></router-view>
     </keep-alive>
-    <router-view v-if="!this.$route.meta.keepAlive" :key="$route.fullPath" ></router-view>
-    <!-- <keep-alive>
-      <router-view :key="$route.fullPath"></router-view>
-    </keep-alive> -->
+    <router-view
+      v-if="!this.$route.meta.keepAlive"
+      :key="$route.fullPath"
+    ></router-view>
   </div>
 </template>
 
