9d4d70949101329af7635190082cdd966518dd25
luo-h-x <1877430627@qq.com>
2020-08-25 19:27:45 +0800

    index

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -8,7 +8,7 @@
         <el-aside width="200px">
           <Sidebar />
         </el-aside>
-        <el-scrollbar>
+        <el-scrollbar style="width: 100%;" class="main-scrollbar" ref="elscrollbar">
           <el-main>
             <app-main />
           </el-main>
@@ -32,28 +32,44 @@ export default {
     Footer,
     Sidebar,
     AppMain
+  },
+  watch: {
+    // 监视路由变化返回上次路由位置
+    '$route.fullPath': function(newVal, oldVal) {
+      if (sessionStorage.getItem(newVal) && this.$route.fullPath === newVal) {
+        this.$nextTick(() => {
+          this.$refs.elscrollbar.wrap.scrollTop = sessionStorage.getItem(newVal)
+        })
+      }
+      let top = this.$refs.elscrollbar.wrap.scrollTop
+      sessionStorage.setItem(oldVal, top)
+    }
+  },
+  mounted() {
+    // 获取最外层滚动元素,用于滚动到指定位置
+    this.$nextTick(() => {
+      this.$store.commit('getMainScrollM', this.$refs.elscrollbar.wrap)
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .layout {
-  height: 100vh;
+  width: 144vh;
+  height: 96vh;
+  box-shadow: 0 2px 14px 2px #ccc;
   .el-container {
     height: 100%;
     .el-header {
-      background-color: rgb(77, 72, 72);
       display: flex;
       align-items: center;
-    }
-    .el-scrollbar {
-      width: 100%;
+      padding: 0 !important;
     }
     .el-main {
       background: #fff;
     }
     .el-footer {
-      background-color: rgb(77, 72, 72);
       display: flex;
       align-items: center;
     }
