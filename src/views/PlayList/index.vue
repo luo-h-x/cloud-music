b81b35d560be9b2809cc932111e65083fc512d14
luo-h-x <1877430627@qq.com>
2020-08-28 16:06:22 +0800

    添加loading效果

--- src/views/PlayList/index.vue
+++ src/views/PlayList/index.vue
@@ -2,7 +2,10 @@
   <div class="playList-page" v-if="detailInfo">
     <!-- 歌单信息 -->
     <div class="p-data">
-      <img :src="detailInfo.coverImgUrl" class="data-photo" />
+      <img
+        :src="detailInfo.coverImgUrl + '?param=400y400'"
+        class="data-photo"
+      />
       <div class="p-content">
         <!-- 标题 -->
         <div class="p-main">
@@ -34,7 +37,7 @@
         </div>
         <!-- 操作 -->
         <div class="p-actions">
-          <a href="javascript:;" class="p-btn btn-allplay"
+          <a @click="playSong(0)" href="javascript:;" class="p-btn btn-allplay"
             ><img
               src="../../assets/play.svg"
               class="p-icon img-play"
@@ -72,74 +75,61 @@
     </div>
     <!-- 歌单列表 -->
     <div class="p-list">
-      <el-table :data="tableData" stripe border style="width: 100%">
-        <!-- <el-table-column prop="number" label=""> </el-table-column> -->
-        <el-table-column type="index"> </el-table-column>
-        <el-table-column label="操作">
-          <template>
-            <img class="p-icon" src="../../assets/like.svg" alt="喜欢" />
-            <img class="p-icon" src="../../assets/download.svg" alt="下载" />
-            <img class="p-icon" src="../../assets/share.svg" alt="下载" />
-          </template>
-        </el-table-column>
-        <el-table-column prop="name" label="音乐标题"> </el-table-column>
-        <el-table-column prop="ar[0].name" label="歌手"> </el-table-column>
-        <el-table-column prop="al.name" label="专辑"> </el-table-column>
-        <el-table-column prop="dt" label="时长"> </el-table-column>
-      </el-table>
+      <div class="songs-item header">
+        <span></span>
+        <span>标题</span>
+        <span>歌手</span>
+        <span>专辑</span>
+        <span>时长</span>
+      </div>
+      <ul class="songs">
+        <li class="songs-item" v-for="(item, index) in tableData" :key="index">
+          <span class="num">{{ index + 1 }}</span>
+          <a
+            @dblclick="playSong(index)"
+            href="javascript:;"
+            title="双击播放"
+            class="tit"
+            >{{ item.name }}</a
+          >
+          <router-link
+            :to="{ path: '/Singer', query: { id: item.ar[0].id } }"
+            class="sa-title"
+            >{{ item.ar[0].name }}</router-link
+          >
+          <router-link
+            :to="{ path: '/Album', query: { id: item.al.id } }"
+            class="sa-title"
+            >{{ item.al.name }}</router-link
+          >
+          <span class="dt">{{ item.dt }}</span>
+        </li>
+      </ul>
     </div>
     <!-- 评论 -->
-    <div class="p-comment" v-if="comment">
-      <div class="c-top">
-        <h2 class="c-title">评论</h2>
-        <span class="c-count">共{{ comment.total }}条评论</span>
-      </div>
-      <!-- 输入评论 -->
-      <div class="c-input">
-        <el-input
-          type="textarea"
-          placeholder="期待你的神评论……"
-          v-model="textarea"
-          maxlength="140"
-          show-word-limit
-          :rows="3"
-        >
-        </el-input>
-        <a href="javascript:;" class="c-replay" @click="toComment">发表评论</a>
-      </div>
-      <!-- 精彩评论 -->
-      <hot-comments
-        v-if="comment.hotComments.length > 0"
-        :hotComments="comment.hotComments"
-        @addpraise="addpraise"
-        @toCommentReply="toCommentReply"
-      />
-      <!-- 最新评论 -->
-      <Comments
-        @addpraise="addpraise"
-        @toCommentReply="toCommentReply"
-        :comments="comment.comments"
-      />
-    </div>
+    <comments />
   </div>
+  <div
+    class="load"
+    v-loading="true"
+    element-loading-text="努力加载中"
+    element-loading-background="#fff"
+    v-else
+  ></div>
 </template>
 
 <script>
 import api from '../../api/discover'
 import utils from '../../utils/common'
-import Comments from './Comments'
-import HotComments from './HotComments'
+import Comments from '../../components/Comments'
 export default {
   components: {
-    Comments,
-    HotComments
+    Comments
   },
   data() {
     return {
       tableData: null,
-      textarea: '',
-      detailInfo: null,
-      comment: null
+      detailInfo: null
     }
   },
   mounted() {
@@ -155,122 +145,35 @@ export default {
         item.dt = utils.formatDay(item.dt)
       })
     })
-    api.getComments(this.$route.query.id).then(val => {
-      this.comment = val.data
-      // 时间戳 => 日期
-      this.comment.hotComments.forEach(item => {
-        item.time = utils.formatDate(item.time, 1)
-      })
-      this.comment.comments.forEach(item => {
-        item.time = utils.formatDate(item.time, 1)
-      })
-    })
   },
   methods: {
-    // 添加评论
-    toComment() {
-      if (this.textarea) {
-        // 添加评论
-        this.comment.comments.unshift({
-          content: this.textarea,
-          likedCount: 0,
-          commentId: this.comment.comments.length + 1,
-          time: new Date().toLocaleString(),
-          beReplied: [],
-          user: {
-            nickname: '默认名',
-            avatarUrl:
-              'http://p1.music.126.net/KxePid7qTvt6V2iYVy-rYQ==/109951165050882728.jpg',
-            userId: 1
-          }
-        })
-        // 评论数加一
-        this.comment.total += 1
-        // 清空评论
-        this.textarea = ''
-        // 通知
-        this.$notify({
-          title: '成功',
-          message: '评论成功',
-          type: 'success',
-          duration: 2000,
-          position: 'top-left'
-        })
-      } else {
-        this.$alert('请输入内容!!', '', {
-          confirmButtonText: '确定'
-        })
-      }
-    },
-    // 点赞
-    addpraise(index, type) {
-      if (type === 'comments') {
-        !this.comment.comments[index].ispraise
-          ? (this.comment.comments[index].likedCount -= 1) // 取消点赞
-          : (this.comment.comments[index].likedCount += 1)
-      } else {
-        !this.comment.hotComments[index].ispraise
-          ? (this.comment.hotComments[index].likedCount -= 1) // 取消点赞
-          : (this.comment.hotComments[index].likedCount += 1)
-      }
-    },
-    // @回复评论
-    toCommentReply(item, text) {
-      if (text) {
-        // 添加评论
-        this.comment.comments.unshift({
-          content: text,
-          likedCount: 0,
-          commentId: this.comment.comments.length + 10,
-          time: new Date().toLocaleString(),
-          beReplied: [
-            {
-              user: {
-                nickname: item.user.nickname
-              },
-              content: item.content
-            }
-          ],
-          user: {
-            nickname: '默认名',
-            avatarUrl:
-              'http://p1.music.126.net/KxePid7qTvt6V2iYVy-rYQ==/109951165050882728.jpg',
-            userId: 1
-          }
-        })
-        // 评论数加一
-        this.comment.total += 1
-        // 清空评论
-        this.textarea = ''
-        // 通知
-        this.$notify({
-          title: '成功',
-          message: '评论成功',
-          type: 'success',
-          duration: 2000,
-          position: 'top-left'
-        })
-      } else {
-        this.$alert('请输入内容!!', '', {
-          confirmButtonText: '确定'
-        })
-      }
-    },
+    // 标签歌单
     toTag(tag) {
-      api.getTag(tag).then(val => {
-        let data = val.data.playlists
-        data.forEach(item => {
-          item.playCount = utils.countToString(item.playCount)
-        })
-        sessionStorage.setItem(tag, JSON.stringify(data))
-        this.$router.push('/Discover/SongList')
+      this.$router.push({
+        path: '/Discover/SongList',
+        query: { cat: tag, limit: 32 }
       })
+    },
+    // 播放音乐
+    playSong(index) {
+      let data = {}
+      data.currentIndex = index
+      data.ids = this.tableData[0].id
+      this.tableData.forEach(item => {
+        data.ids += ',' + item.id
+      })
+      this.$store.dispatch('querySongsA', data)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.load {
+  width: 100%;
+  height: calc(100vh - 120px);
+  position: absolute;
+}
 .playList-page {
   // 歌单信息
   .p-data {
@@ -295,9 +198,9 @@ export default {
           font-size: 20px;
           font-weight: normal;
           margin: 0 20px;
-          width: 420px;
         }
         .p-count {
+          width: 150px;
           display: flex;
           color: #ccc;
           .left {
@@ -387,147 +290,37 @@ export default {
     margin-right: 5px;
     cursor: pointer;
   }
-  // 歌单列表
-  .p-list {
-    margin: 20px 0;
+  // 歌单
+  .songs-item {
+    display: grid;
+    grid-template-columns: 0.5fr 3fr 1.5fr 3fr 1fr;
+    grid-column-gap: 10px;
+    align-items: center;
+    margin: 10px 0;
+    font-size: 14px;
+    color: #999;
+    height: 50px;
+    &:nth-child(even) {
+      background: #fafafa;
+    }
+    &:hover {
+      background: rgba(0, 0, 0, 0.1) !important;
+    }
+    .sa-pic {
+      width: 50px;
+    }
+    .tit {
+      cursor: default;
+    }
   }
-  // 评论
-  .p-comment {
-    display: flex;
-    flex-direction: column;
-    .c-top {
-      display: flex;
-      align-items: center;
-      .c-title {
-        font-size: 24px;
-        font-weight: 400;
-        line-height: 58px;
-      }
-      .c-count {
-        color: #999;
-        font-size: 14px;
-        margin-left: 12px;
-      }
-    }
-    // 输入评论
-    .c-input {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      .c-replay {
-        line-height: 27px;
-        border: 1px solid #31c27c;
-        background-color: #31c27c;
-        color: #fff;
-        margin: 10px 0;
-      }
-    }
-    // 评论类型
-    ::v-deep .c-type {
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 34px;
-      border-bottom: 1px #ededed solid;
-    }
-    // 评论列表
-    ::v-deep .h-list {
-      font-size: 14px;
-      .h-list-item {
-        display: flex;
-        align-items: center;
-        border-bottom: 1px #ededed solid;
-        margin: 10px 0;
-        // 头像
-        .h-avatar {
-          width: 38px;
-          height: 38px;
-          border-radius: 999px;
-          cursor: pointer;
-        }
-        // 详情
-        .h-details {
-          width: 100%;
-          display: flex;
-          flex-direction: column;
-          margin-left: 20px;
-          .hd-name {
-            color: #999;
-            line-height: 28px;
-          }
-          // 普通评论
-          .hd-content {
-            line-height: 28px;
-            width: 80%;
-          }
-          .hd-info {
-            display: flex;
-            justify-content: space-between;
-            .hd-time {
-              line-height: 28px;
-              color: #999;
-            }
-            .hd-option {
-              a {
-                margin-right: 10px;
-                &:hover {
-                  color: #2caf6f;
-                }
-              }
-              .hd-praise {
-                color: #2caf6f;
-              }
-            }
-          }
-          // 回复评论
-          .hd-reply {
-            .hdr-content {
-              width: 80%;
-              .hdr-nickname {
-                color: #31c27c;
-              }
-              .hdr-subcomment {
-                line-height: 28px;
-              }
-            }
-            .hdr-history {
-              border-left: 1px #999 solid;
-              margin: 5px 0 4px 12px;
-              padding: 0 0 0 10px;
-              color: #999;
-              width: 80%;
-            }
-          }
-          // @回复
-          .hd-input {
-            display: none;
-            flex-direction: column;
-            align-items: flex-end;
-            .hdi-option {
-              display: flex;
-              .hdi-reply {
-                width: 50px;
-                line-height: 27px;
-                text-align: center;
-                border: 1px solid #31c27c;
-                background-color: #31c27c;
-                color: #fff;
-                margin: 10px 5px;
-              }
-              .hdi-cancel {
-                width: 50px;
-                line-height: 27px;
-                text-align: center;
-                border: 1px solid #999;
-                margin: 10px 5px;
-              }
-            }
-          }
-          // 显示回复框
-          .hdi-show {
-            display: flex;
-          }
-        }
-      }
+  // 标题
+  .header {
+    margin-top: 20px;
+    border: 1px solid #ccc;
+    & > span {
+      line-height: 50px;
+      padding-left: 10px;
+      border-left: 1px solid #ccc;
     }
   }
 }
