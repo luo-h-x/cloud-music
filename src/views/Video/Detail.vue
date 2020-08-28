4ac6a9371fb26b691c963e91dc15ea4e841a9475
luo-h-x <1877430627@qq.com>
2020-08-28 16:08:34 +0800

    删除评论axios

--- src/views/Video/Detail.vue
+++ src/views/Video/Detail.vue
@@ -1,56 +1,61 @@
 <template>
   <div class="v-detail" v-if="detail">
-    <div style="display: flex;">
-      <div class="left">
-        <!-- 标题 -->
-        <div class="tit">
-          <el-tag type="danger">MV</el-tag>
-          <h3 class="tc">{{ detail.data.name }}</h3>
-          <router-link
-            :to="{ path: '/Singer', query: { id: detail.data.artistId } }"
-            class="name"
-            >{{ detail.data.artistName }}</router-link
-          >
-        </div>
-        <!-- 视频 -->
-        <video
-          controls
-          style="width:550px;height: 310px;"
-          autoplay
-          :src="url"
-        ></video>
+    <div class="left">
+      <!-- 标题 -->
+      <div class="tit">
+        <el-tag type="danger">MV</el-tag>
+        <h3 class="tc">{{ detail.data.name }}</h3>
+        <router-link
+          :to="{ path: '/Singer', query: { id: detail.data.artistId } }"
+          class="name"
+          >{{ detail.data.artistName }}</router-link
+        >
       </div>
-      <div class="right">
-        <div class="introduce">
-          <div>
-            <h3 class="tit">视频介绍</h3>
-            <p class="info">
-              <span class="time">{{ detail.data.publishTime }}</span>
-              <span class="count">播放次数:{{ detail.data.playCount }}</span>
-            </p>
-            <p class="content">
-              简介:
-              {{ detail.data.desc }}
-            </p>
-            <!-- <div class="tags">
+      <!-- 视频 -->
+      <video
+        controls
+        style="width:800px;height: 450px;"
+        autoplay
+        :src="url"
+        @play="playV"
+      ></video>
+    </div>
+    <div class="right">
+      <div class="introduce">
+        <div>
+          <h3 class="tit">视频介绍</h3>
+          <p class="info">
+            <span class="time">发布时间: {{ detail.data.publishTime }}</span>
+            <span class="count">播放次数: {{ detail.data.playCount }}</span>
+          </p>
+          <p class="content">
+            简介:
+            {{ detail.data.desc }}
+          </p>
+          <!-- <div class="tags">
             <span>标签:</span>
             <a class="tag">动漫</a>
             <a class="tag">影视</a>
           </div> -->
-          </div>
-          <!-- 相关推荐 -->
-          <div class="recommend"></div>
         </div>
+        <!-- 相关推荐 -->
+        <div class="recommend"></div>
       </div>
     </div>
     <!-- 评论 -->
     <comments />
   </div>
+  <div
+    class="load"
+    v-loading="true"
+    element-loading-text="努力加载中"
+    element-loading-background="#fff"
+    v-else
+  ></div>
 </template>
 
 <script>
 import api from '../../api/discover'
-import utils from '../../utils/common'
 import Comments from '../../components/Comments'
 export default {
   components: { Comments },
@@ -64,40 +69,49 @@ export default {
     // mv url
     api.getUrlMv(this.$route.query.id).then(val => {
       this.url = val.data.data.url
+      if (!this.url) {
+        this.$notify({
+          title: '加载失败',
+          message: val.data.data.msg,
+          type: 'warning',
+          duration: 4000,
+          position: 'top-left'
+        })
+      }
     })
     // mv 信息
     api.getMvD(this.$route.query.id).then(val => {
       this.detail = val.data
     })
-    // 评论
-    api.getMvC(this.$route.query.id).then(val => {
-      let comments = val.data
-      // 时间戳 => 日期
-      comments.hotComments.forEach(item => {
-        item.time = utils.formatDate(item.time, 1)
-      })
-      comments.comments.forEach(item => {
-        item.time = utils.formatDate(item.time, 1)
-      })
-      this.$store.dispatch('queryCommetsA', comments)
-    })
+    // 暂停音乐
     let audio = document.getElementsByClassName('audio')[0]
-    audio.pause()
+    if (audio) {
+      audio.pause()
+    }
   },
-  beforeDestroy() {
-    // let audio = document.getElementsByClassName('audio')[0]
-    // audio.play()
+  methods: {
+    playV() {
+      // 暂停音乐
+      let audio = document.getElementsByClassName('audio')[0]
+      if (audio) {
+        audio.pause()
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.load {
+  width: 100%;
+  height: calc(100vh - 120px);
+  position: absolute;
+}
 .v-detail {
   display: flex;
   flex-direction: column;
 }
 .left {
-  width: 550px;
   display: flex;
   flex-direction: column;
   .tit {
