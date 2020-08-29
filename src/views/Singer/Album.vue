226f95ea3e597ca5af6b9265b1c04f367e9fd7c1
luo-h-x <1877430627@qq.com>
2020-08-29 17:33:43 +0800

    图片优化

--- src/views/Singer/Album.vue
+++ src/views/Singer/Album.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="singer-mv-page">
-    <ul class="singer-album">
-      <li
-        class="singer-album-item"
-        v-for="(item, index) in album.hotAlbums"
-        :key="index"
-      >
-        <img :src="item.picUrl" class="sa-pic" />
-        <!-- <router-link
-            :to="{ path: '/PlayList', query: { id: item.id } }"
+    <div
+      class="load"
+      v-loading="loading"
+      element-loading-text="努力加载中"
+      element-loading-background="#fff"
+      v-if="loading"
+    ></div>
+    <div :class="{ hide: loading }">
+      <ul class="singer-album">
+        <li
+          class="singer-album-item"
+          v-for="(item, index) in album.hotAlbums"
+          :key="index"
+        >
+          <img @load="show" :src="item.picUrl + '?param=100y100'" class="sa-pic" />
+          <router-link
+            :to="{ path: '/Album', query: { id: item.id } }"
             class="sa-title"
-            >我不服</router-link
-          > -->
-        <a href="javascript:;" class="sa-title">{{ item.name }}</a>
-        <span class="sa-count">歌曲: {{ item.size }}首</span>
-        <span class="sa-time">发行时间: {{ item.publishTime }}</span>
-      </li>
-    </ul>
+            >{{ item.name }}</router-link
+          >
+          <!-- <a href="javascript:;" class="sa-title">{{ item.name }}</a> -->
+          <span class="sa-count">歌曲: {{ item.size }}首</span>
+          <span class="sa-time">发行时间: {{ item.publishTime }}</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -25,7 +34,9 @@ import api from '../../api/singer'
 export default {
   data() {
     return {
-      album: []
+      album: [],
+      loading: true,
+      count: 0
     }
   },
   mounted() {
@@ -35,11 +46,27 @@ export default {
         item.publishTime = new Date(item.publishTime).toLocaleDateString()
       })
     })
+  },
+  methods: {
+    show() {
+      this.count++
+      if (this.count === this.album.hotAlbums.length) {
+        this.loading = false
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.hide {
+  display: none;
+}
+.load {
+  width: 100%;
+  height: calc(100vh - 435px);
+  position: absolute;
+}
 .singer-album {
   .singer-album-item {
     display: grid;
@@ -48,6 +75,7 @@ export default {
     margin: 10px 0;
     font-size: 14px;
     color: #999;
+    height: 50px;
     .sa-pic {
       width: 50px;
     }
